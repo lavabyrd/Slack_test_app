92b8793764521046f5a66bc54beb604bae095d1a
Mark Preston <mpreston@slack-corp.com>
2017-09-04 15:47:43 +0100

    updated endpoints

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
-from flask import Flask, request
-import requests
+from flask import Flask, request,jsonify, json, Response
+
 app = Flask(__name__)
 
 
 # just to add basic testing variables
+example_text = {
+    "text": "Would you like to play a game?",
+    "attachments": [
+        {
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "game",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "game",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                	},
+                {
+                    "name": "game",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+        }
+    ]
+}
 
-
-
-@app.route("/")
-def hello():
-    return "Hello World!"
 
 @app.errorhandler(500)
 def page_not_found(error):
-    return 'This page does not exist', 404
+    return 'This page does not exist', 500
 
 
 @app.route("/oauth")
@@ -24,9 +58,36 @@ def auth_route():
 	r = requests.get(url, pay)
 	return 'that works'
 
-@app.route("/test", methods = ['POST'])
+@app.route("/standard_message", methods = ['POST'])
 def test_route():
-	return 'Does this work, or not...'
+	return jsonify(example_text)
+
+@app.route("/output", methods = ['POST'])
+def output_route():
+
+	    # Parse the request payload
+    form_json = json.loads(request.form["payload"])
+    
+
+    # Check to see what the user's selection was and update the message
+    selection = form_json["actions"][0]["value"]
+    
+
+    if selection == "war":
+        message_text = "The only winning move is not to play.\nHow about a nice game of chess?"
+    else:
+        message_text = ":horse:"
+
+    response = slack_client.api_call(
+      "chat.update",
+      channel=form_json["channel"]["id"],
+      ts=form_json["message_ts"],
+      text=message_text,
+      attachments=[]
+    )
+
+    return make_response("", 200)
+
 
 if __name__ == '__main__':
     app.run(port=4390)
