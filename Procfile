825796e7e555b6378d32d4c7d0a4daf209bc90f6
splinterific <markgpreston@gmail.com>
2018-02-02 12:45:20 +0000

    updated to move the app run to the main file

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: flask run -p $PORT
+web: python slackbot.py -p $PORT
--- slackbot.py
+++ slackbot.py
@@ -3,3 +3,9 @@ newrelic.agent.initialize()
 
 from app import app
 import api
+import os
+# App startup
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+
+    app.run(debug=True, host='0.0.0.0', port=port)
