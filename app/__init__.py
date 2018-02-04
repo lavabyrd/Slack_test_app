1da13ef8f803fa8a75ecdc30be082c7376214fb7
splinterific <markgpreston@gmail.com>
2018-02-04 09:23:24 +0000

    updated to include sqlalchemy and a model

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from config import Config
 
 
 app = Flask(__name__)
-Bootstrap(app)
 app.config.from_object(Config)
+Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
 
 
