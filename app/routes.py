c70560bddd57f7712aa730fedee3c2d2cdc36a13
splinterific <markgpreston@gmail.com>
2018-02-04 11:09:46 +0000

    updated to include a working install and registeration form"

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-from app import app
-
-from flask import render_template, flash, redirect, url_for
-import requests
+from app import app,db
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
+from app.forms import LoginForm, RegistrationForm
 import os
-from app.forms import LoginForm
 
 client_id = os.environ.get('client_id')
 client_secret = os.environ.get('client_secret')
@@ -11,9 +12,10 @@ client_secret = os.environ.get('client_secret')
 # Just a base homepage to show it working without using Slack
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
     user = {"username": "Mark"}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home', post=posts)
 
 # This is for allowing the installation of the app to a team
 @app.route("/install")
@@ -22,12 +24,39 @@ def add_to_slack():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations you are now a registered User')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/posts')
